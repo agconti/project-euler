13e5d261c2712dd133e5fa421feeca50573d5615
agconti <agc11d@gmail.com>
2015-01-18 21:31:00 -0500

    feat(a/problem_1_multiples_of_3_and_5): updated for filter instead of map

--- problem_1_multiples_of_3_and_5.py
+++ problem_1_multiples_of_3_and_5.py
@@ -19,5 +19,5 @@ def frizz_buzz(number):
     @param n -- a non-negative integer
     @return integer
     """
-    l = lambda x: x if (x % 5 == 0) or (x % 3 == 0) else 0
-    return sum(map(l, range(number)))
+    mutliples_of_three_and_five = lambda x: (x % 5 == 0) or (x % 3 == 0)
+    return sum(filter(mutliples_of_three_and_five, range(number)))
