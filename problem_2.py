6adcc1e29a6fb058130acd282e648a793f830bfe
agconti <agc11d@gmail.com>
2016-04-18 21:10:34 -0400

    tes(problem_2): added basic pytest check

--- problem_2.py
+++ problem_2.py
@@ -1,9 +1,12 @@
+def test_frizz_buzz():
+    assert get_sum_even_fib_terms(10) == 10
+
 def fibonacci(limit):
     a = 0
     b = 1
 
     while a < limit:
-        if i % 2 == 0:
+        if a % 2 == 0:
             yield a
 
         a, b = b, a + b
