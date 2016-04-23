09b5d70ca7a23648cab71d65b6b787ac9629f107
agconti <agc11d@gmail.com>
2016-04-23 14:32:57 -0400

    feat(problem_6): added naive solution

---
+++ problem_6.py
@@ -0,0 +1,10 @@
+def test_sum_square_difference():
+    assert sum_square_difference(1, 10) == 2640
+
+
+def sum_square_difference(low, high):
+    high += 1
+
+    sum_squares = pow(sum(xrange(low, high)), 2)
+    square_sum = sum(map(lambda num: num * num, xrange(low, high)))
+    return sum_squares - square_sum
